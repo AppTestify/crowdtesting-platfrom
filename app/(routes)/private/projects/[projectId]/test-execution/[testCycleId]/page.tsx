472b1c0f0cd83b489ca4ce08a667846edc0031da
Aman377 <aman@excitesys.com>
2024-12-03 19:22:39 +0530

    Fixed: console error

--- app/(routes)/private/projects/[projectId]/test-execution/[testCycleId]/page.tsx
+++ app/(routes)/private/projects/[projectId]/test-execution/[testCycleId]/page.tsx
@@ -3,6 +3,9 @@
 import { ITestCaseResult } from '@/app/_interface/test-case-result';
 import { getTestExecutionsService } from '@/app/_services/test-execution.service';
 import toasterService from '@/app/_services/toaster-service';
+import { Button } from '@/components/ui/button';
+import { ColumnDef, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, SortingState, useReactTable, VisibilityState } from '@tanstack/react-table';
+import { ArrowUpDown } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 
@@ -11,6 +14,75 @@ export default function TestCasesInTestExecution() {
     const [testExecution, setTestExecution] = useState<ITestCaseResult[]>([]);
     const { projectId } = useParams<{ projectId: string }>();
     const { testCycleId } = useParams<{ testCycleId: string }>();
+    const [sorting, setSorting] = useState<SortingState>([]);
+    const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+    const [rowSelection, setRowSelection] = useState({});
+    const [globalFilter, setGlobalFilter] = useState<unknown>([]);
+
+    const columns: ColumnDef<ITestCaseResult>[] = [
+        {
+            accessorKey: "customId",
+            header: ({ column }) => {
+                const isSorted = column.getIsSorted();
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(isSorted === "asc")}
+                    >
+                        ID
+                        <ArrowUpDown />
+                    </Button>
+                );
+            },
+            cell: ({ row }) => (
+                <div className="hover:text-primary cursor-pointer ml-4">
+                    {row.getValue("customId")}</div>
+            ),
+            sortingFn: "alphanumeric"
+        },
+        {
+            accessorKey: "title",
+            header: "Title",
+            cell: ({ row }) => (
+                <div className="capitalize hover:text-primary cursor-pointer">
+                    {row.getValue("title")}</div>
+            ),
+        },
+        {
+            accessorKey: "description",
+            header: "Description",
+            cell: ({ row }) => (
+                <div className="capitalize">{row.getValue("description")}</div>
+            ),
+        },
+        {
+            id: "actions",
+            enableHiding: false,
+            // cell: ({ row }) => (
+            //     <TestExecutionRowActions row={row as Row<ITestCycle>} refreshTestCycle={refreshTestCycle} />
+            // ),
+        },
+    ];
+
+    const table = useReactTable({
+        data: testExecution,
+        columns,
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
+        globalFilterFn: "includesString",
+        state: {
+            sorting,
+            globalFilter,
+            columnVisibility,
+            rowSelection,
+        },
+        onGlobalFilterChange: setGlobalFilter,
+    });
 
     const getTestCycle = async () => {
         setIsLoading(true);
