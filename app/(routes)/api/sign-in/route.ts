5bfea9f264dc69447b1bde1813f422cb8506723d
Shivang chauhan <xshivangchauhanx@gmail.com>
2024-12-07 15:50:18 +0530

    feat: added rich text editor

--- app/(routes)/api/sign-in/route.ts
+++ app/(routes)/api/sign-in/route.ts
@@ -39,6 +39,7 @@ export async function POST(req: Request) {
 
     const { email, password } = response.data;
     const existingUser = await User.findOne({ email });
+    console.log(existingUser);
 
     if (!existingUser) {
       return Response.json(
@@ -51,6 +52,8 @@ export async function POST(req: Request) {
       password,
       existingUser.password
     );
+
+    console.log('isPasswordValid: ', existingUser.password)
 
     if (!isPasswordValid) {
       return Response.json(
