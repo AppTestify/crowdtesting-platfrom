04077fe3da76c979ffd1037361392a9a55a51f78
Aman377 <aman@excitesys.com>
2025-01-08 17:35:14 +0530

    Fixed:issue

--- app/_components/project-layout/_constants/index.ts
+++ app/_components/project-layout/_constants/index.ts
@@ -3,7 +3,7 @@ import { UserRoles } from "@/app/_constants/user-roles";
 export const getProjectTabs = (user: any) => {
   switch (user?.role) {
     case UserRoles.ADMIN:
-      return ["dashboard","test instructions", "users", "issues", "test cycle", "notes"];
+      return ["dashboard","test instructions",  "users", "issues", "test cycle", "notes"];
     case UserRoles.TESTER:
       return ["dashboard","test instructions", "issues", "test cycle", "notes"];
     case UserRoles.CLIENT:
