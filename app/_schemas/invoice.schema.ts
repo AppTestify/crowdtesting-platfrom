ff7ff3edba81f31bfd954733cd323b4bb16edaa2
Aman377 <aman@excitesys.com>
2025-02-17 15:48:11 +0530

    Fix:build error

--- app/_schemas/invoice.schema.ts
+++ app/_schemas/invoice.schema.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
+import { File } from "buffer";
 
 export const invoiceSchema = z.object({
-  file: z.instanceof(File).refine((file) => file.size <= 15 * 1024 * 1024, {
-    message: "Document must be under 15 MB",
-  }),
+  file: z.instanceof(File),
 });
